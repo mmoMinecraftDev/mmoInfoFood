3ad4f882e7ed5aa4fa2e230c7c9fca3c0e82daa3
mcsnetworks <mike@mcsnetworks.com>
2012-10-21 21:55:29 -0500

    OnTick() = Bad.  Dont use them unless you have to. Implemented new Bar GUI

--- src/main/java/mmo/Info/mmoInfoFood.java
+++ src/main/java/mmo/Info/mmoInfoFood.java
@@ -17,31 +17,44 @@
 package mmo.Info;
 
 import java.util.HashMap;
+import java.util.Map;
+
 import mmo.Core.InfoAPI.MMOInfoEvent;
 import mmo.Core.MMOPlugin;
 import mmo.Core.MMOPlugin.Support;
 import mmo.Core.util.EnumBitSet;
 
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.FoodLevelChangeEvent;
 import org.bukkit.event.player.PlayerEvent;
 import org.bukkit.event.player.PlayerGameModeChangeEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.plugin.PluginManager;
+import org.getspout.spoutapi.gui.Color;
+import org.getspout.spoutapi.gui.ContainerType;
+import org.getspout.spoutapi.gui.GenericContainer;
+import org.getspout.spoutapi.gui.GenericGradient;
 import org.getspout.spoutapi.gui.GenericLabel;
+import org.getspout.spoutapi.gui.GenericTexture;
+import org.getspout.spoutapi.gui.Gradient;
 import org.getspout.spoutapi.gui.InGameHUD;
 import org.getspout.spoutapi.gui.Label;
+import org.getspout.spoutapi.gui.RenderPriority;
 import org.getspout.spoutapi.gui.Screen;
+import org.getspout.spoutapi.gui.Texture;
 import org.getspout.spoutapi.player.SpoutPlayer;
 
 public class mmoInfoFood extends MMOPlugin implements Listener {
-	private HashMap<Player, CustomLabel> widgets = new HashMap();	
-	
+	private HashMap<Player, CustomLabel> widgets = new HashMap();
+	private static final Map<Player, CustomWidget> foodbar = new HashMap<Player, CustomWidget>();
+	private static String config_displayas = "bar";
+
 	@Override
-	public EnumBitSet mmoSupport(EnumBitSet support) {
-		support.set(Support.MMO_NO_CONFIG);
+	public EnumBitSet mmoSupport(EnumBitSet support) {		
 		support.set(Support.MMO_AUTO_EXTRACT);
 		return support;
 	}
@@ -51,17 +64,28 @@ public class mmoInfoFood extends MMOPlugin implements Listener {
 		super.onEnable();
 		pm.registerEvents(this, this);
 	}
-	
+
+	@Override
+	public void loadConfiguration(final FileConfiguration cfg) {
+		config_displayas = cfg.getString("displayas", config_displayas);		
+	}
+
 	@EventHandler
 	public void onMMOInfo(MMOInfoEvent event)
 	{
 		if (event.isToken("food")) {
 			SpoutPlayer player = event.getPlayer();
 			if (player.hasPermission("mmo.info.food")) {
-				player.getMainScreen().getHungerBar().setVisible(false);
-				CustomLabel label = (CustomLabel)new CustomLabel().setResize(true).setFixed(true);
-				this.widgets.put(player, label);
-				event.setWidget(this.plugin, label);
+				if (config_displayas.equalsIgnoreCase("bar")) {				
+					final CustomWidget widget = new CustomWidget();
+					foodbar.put(player, widget);
+					event.setWidget(plugin, widget);
+				} else { 
+					player.getMainScreen().getHungerBar().setVisible(false);
+					CustomLabel label = (CustomLabel)new CustomLabel().setResize(true).setFixed(true);
+					this.widgets.put(player, label);
+					event.setWidget(this.plugin, label);
+				}
 				event.setIcon("hunger.png");			
 			}
 		}
@@ -69,20 +93,35 @@ public class mmoInfoFood extends MMOPlugin implements Listener {
 
 	public class CustomLabel extends GenericLabel
 	{
-		private boolean check = true;
-
-		public CustomLabel() {
-		}
-
-		public void change() {
-			this.check = true;
-		}
-		private transient int tick = 0;
-		public void onTick()
-		{
-			if (tick++ % 20 == 0) {
+		@EventHandler
+		public void onMMOFoodChange(FoodLevelChangeEvent event) {						
+			if (event instanceof Player) {
 				setText(String.format(getScreen().getPlayer().getFoodLevel() + "/20"));
 			}
+		}
+	}
+
+	public class CustomWidget extends GenericContainer {
+
+		private final Gradient slider = new GenericGradient();
+		private final Texture bar = new GenericTexture();
+
+		public CustomWidget() {
+			super();
+			slider.setMargin(1).setPriority(RenderPriority.Normal).setHeight(5).setWidth(20).shiftXPos(1).shiftYPos(1);
+			bar.setUrl("bar10.png").setPriority(RenderPriority.Lowest).setHeight(7).setWidth(103).shiftYPos(0);			
+			this.setLayout(ContainerType.OVERLAY).setMinWidth(100).setMaxWidth(100);
+			this.addChildren(slider, bar);
+		}
+
+		@EventHandler
+		public void onMMOFoodChange(FoodLevelChangeEvent event) {
+			final int playerFood = (int) (getScreen().getPlayer().getFoodLevel()*5);						
+			if (playerFood>=33) {			
+				slider.setColor(new Color(0,0,1f,1f)).setWidth(playerFood); //Blue				
+			} else if (playerFood<33) {
+				slider.setColor(new Color(0.69f,0.09f,0.12f,1f)).setWidth(playerFood);  //Red				
+			}			
 		}
 	}
 }
